196ad965a58ca378fb5bdbd85fed8590f386d94d
Michał Litwin <michaleklitwin@gmail.com>
2021-07-31 21:42:51 +0200

    added the copied tp command parser

--- waypoint.py
+++ waypoint.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 from collections import namedtuple
+from math import floor
 
 Vec3 = namedtuple("Vec3", ["x", "y", "z"])
 
+class NotACopiedLocation(Exception):
+    def __init__(self, copied: str) -> None:
+        super().__init__(f"'{copied}' is not a F3+C teleport command")
+
 class Waypoint():
+    @staticmethod
+    def from_copied(copied: str):
+        if not copied.startswith("/execute in "): raise NotACopiedLocation(copied)
+        arguments = copied.split(" ")[2:]
+        if len(arguments) < 9: raise NotACopiedLocation(copied)
+
+        try:
+            x = floor(float(arguments[4]))
+            y = floor(float(arguments[5]))
+            z = floor(float(arguments[6]))
+        except ValueError:
+            raise NotACopiedLocation(copied)
+        
+        return __class__(x, y, z, dimension=arguments[0])
+        
+    
     def __init__(self, x: int, y: int, z: int, name="unnamed", dimension="minecraft:overworld") -> None:
         self.position = Vec3(x, y, z)
         self.name = name
@@ -14,6 +35,8 @@ class Waypoint():
         return f"{self.position.x} {self.position.y} {self.position.z} {self.dimension}\t{self.name}"
 
 if __name__ == "__main__":
-    test_waypoint = Waypoint(-10, 60, 20, name="my waypoint", dimension="minecraft:the_end")
+    test_copied = "/execute in minecraft:overworld run tp @s 106.35 73.00 71.35 -1733.88 12.00"
+    print(test_copied)
+    test_waypoint = Waypoint.from_copied(test_copied)
     print(repr(test_waypoint))
     print(str(test_waypoint))
